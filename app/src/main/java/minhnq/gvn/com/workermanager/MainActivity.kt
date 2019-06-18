42af01714b0c15a9c84002efa51fc712c38da18d
Minh <aoeminh@gmail.com>
2019-06-18 17:14:57 +0700

    init

---
+++ app/src/main/java/minhnq/gvn/com/workermanager/MainActivity.kt
@@ -0,0 +1,12 @@
+package minhnq.gvn.com.workermanager
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
