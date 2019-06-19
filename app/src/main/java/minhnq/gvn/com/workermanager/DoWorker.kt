1d8642f542b392446c0718028485104e97bab043
Minh <aoeminh@gmail.com>
2019-06-19 14:49:32 +0700

    Revert "demo simple"
    This reverts commit 625aaee3c59b5597942dd58547a0e8fb18bc8efd.

--- app/src/main/java/minhnq/gvn/com/workermanager/DoWorker.kt
+++ app/src/main/java/minhnq/gvn/com/workermanager/DoWorker.kt
@@ -1,24 +1,4 @@
 package minhnq.gvn.com.workermanager
 
-import android.content.Context
-import android.util.Log
-import androidx.work.Worker
-import androidx.work.WorkerParameters
-
-class DoWorker(val context: Context, val workerParameters: WorkerParameters) : Worker(context, workerParameters) {
-    override fun doWork(): Result {
-        val a = 1
-        val b = 2
-        val c = a + b
-        val data  = inputData.getString("data")
-        Log.d("worker",data)
-
-        return Result.success()
-    }
-
-    override fun onStopped() {
-        super.onStopped()
-        Log.d("worker","onStop")
-
-    }
+class DoWorker  {
 }
