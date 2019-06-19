625aaee3c59b5597942dd58547a0e8fb18bc8efd
Minh <aoeminh@gmail.com>
2019-06-19 14:40:56 +0700

    demo simple

--- app/src/main/java/minhnq/gvn/com/workermanager/DoWorker.kt
+++ app/src/main/java/minhnq/gvn/com/workermanager/DoWorker.kt
@@ -1,4 +1,24 @@
 package minhnq.gvn.com.workermanager
 
-class DoWorker  {
+import android.content.Context
+import android.util.Log
+import androidx.work.Worker
+import androidx.work.WorkerParameters
+
+class DoWorker(val context: Context, val workerParameters: WorkerParameters) : Worker(context, workerParameters) {
+    override fun doWork(): Result {
+        val a = 1
+        val b = 2
+        val c = a + b
+        val data  = inputData.getString("data")
+        Log.d("worker",data)
+
+        return Result.success()
+    }
+
+    override fun onStopped() {
+        super.onStopped()
+        Log.d("worker","onStop")
+
+    }
 }
--- app/src/main/java/minhnq/gvn/com/workermanager/MainActivity.kt
+++ app/src/main/java/minhnq/gvn/com/workermanager/MainActivity.kt
@@ -2,11 +2,83 @@ package minhnq.gvn.com.workermanager
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import androidx.work.*
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.concurrent.TimeUnit
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    var workerManager: WorkManager? = null
+    var workRequest: OneTimeWorkRequest? = null
+    var periodRequest: PeriodicWorkRequest? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        workerManager = WorkManager.getInstance()
+        btn_dowork.setOnClickListener(this)
+        btn_dowork_constrain.setOnClickListener(this)
+        btn_cancel.setOnClickListener(this)
+        btn_cancel_all.setOnClickListener(this)
+        val source = Data.Builder()
+            .putString("data","test")
+            .build()
+    }
+
+    override fun onClick(v: View?) {
+        when (v) {
+            btn_dowork -> doWork()
+            btn_dowork_constrain -> doWorkConstrain()
+            btn_cancel -> cancel()
+            btn_cancel_all -> cancelAll()
+        }
+    }
+
+    private fun cancelAll() {
+        workerManager?.cancelAllWork()
+    }
+
+    fun doWork() {
+
+        val source = Data.Builder()
+            .putString("data","test")
+            .build()
+        periodRequest = PeriodicWorkRequest.Builder(DoWorker::class.java, 15, TimeUnit.MINUTES)
+            .setInputData(source)
+            .build()
+        val workRequestId = periodRequest?.id
+        Log.d("UUID", "$workRequestId")
+
+        workerManager?.enqueue(periodRequest!!)
+
+    }
+
+    fun doWorkConstrain() {
+        val constrain = Constraints.Builder()
+            .setRequiredNetworkType(NetworkType.CONNECTED)
+            .build()
+        workRequest = OneTimeWorkRequest.Builder(DoWorker::class.java)
+            .setConstraints(constrain)
+            .build()
+        val workRequestId = workRequest?.id
+        Log.d("UUID", "$workRequestId")
+        workerManager?.enqueue(workRequest!!)
+        workerManager?.enqueue(workRequest!!)
+    }
+    
+    fun cancel() {
+        val workRequestId = workRequest?.id
+        val periodRequestId = periodRequest?.id
+        workRequestId?.let {
+            workerManager?.cancelWorkById(it)
+            Log.d("UUID", "$it")
+        }
+
+        periodRequestId?.let {
+            workerManager?.cancelWorkById(it)
+            Log.d("UUID", "$it")
+        }
     }
 }
